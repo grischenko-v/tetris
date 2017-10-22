d40486a275d4494c9cfef17a4b9508b6bf49426e
Vladimir <boooobka@gmail.com>
2017-10-22 22:04:56 +0400

    element dark style added

--- src/Element/Element.js
+++ src/Element/Element.js
@@ -3,15 +3,14 @@ import './Element.css';
 
 class Element extends Component {
   constructor(props){
-    super(props);
-    this.value = this.props.value;
+    super(props);   
     this.posX = this.props.posX;
     this.posY = this.props.posY;  
   }
 
   render() {
     return (
-      <div className = "element">
+      <div className = {`element ${this.props.active ? "dark" : ""}`}>
        
       </div>
     );
