cb54ed2078c7ab08be23b574e2dbd6c94854c7b0
Vladimir <boooobka@gmail.com>
2017-11-01 22:05:11 +0400

    add simple animation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   constructor(){
   	super();
   	this.frameCount = 0;
-  	this.fps = 60;
+  	this.fps = 100;
   	this.sizeX = 10;
   	this.sizeY = 20;
   	this.hash = this.initGrid();
@@ -41,11 +41,12 @@ class App extends Component {
  };
  
  animationloop(){
- 	let  newGrid = this.state.grid;
+ 	let  newGrid = this.hash;
     if(this.frameCount < this.fps) this.frameCount++;
     else{
-         if(!this.state.currentFigure) this.createFigure();
-    	 else this.moveDown();  	   	  
+    	this.frameCount = 0;
+        if(!this.state.currentFigure) this.createFigure();
+    	else this.moveDown();  	   	  
     }
     this.setState({    
        grid:  newGrid,
@@ -54,19 +55,29 @@ class App extends Component {
  };
 
  createFigure(){
- let figurePos = [];
- let temp = this.indexToPosition(5);
- figurePos.push(temp);
-   this.hash["05"] = <Element posX = {0} posY = {5} key = {5} active = {true}/>;
+   let figurePos = [];
+   let temp = this.indexToPosition(5);
+   figurePos.push(temp);   
+   console.log(123);
+   this.hash[temp.index] = <Element posX = {temp.X} posY = {temp.Y} key = {5} active = {true}/>;
    this.setState({    
         currentFigure: figurePos
     });
  };
 
  moveDown(){
+   let temp;
+   let figurePos = [];
    if(true){
-      this.hash[this.state.currentFigure.index] = <Element posX = {0} posY = {5} key = {5} active = {false}/>;
-     // console.log(this.state.currentFigure);
+   	  temp = (this.hash[this.state.currentFigure[0].index].props.posX + 1) + "" + this.hash[this.state.currentFigure[0].index].props.posY;
+   	  figurePos.push(temp);
+      console.log(temp);
+      this.hash[this.state.currentFigure[0].index] = <Element posX = {this.state.currentFigure[0].X} posY = {this.state.currentFigure[0].Y}  key = {5} active = {false}/>;
+      
+      this.hash[temp] = <Element posX = {this.state.currentFigure[0].X + 1} posY = {this.state.currentFigure[0].Y}  key = {12313} active = {true}/>;
+      // this.hash[this.state.currentFigure[0].index].props.posX;
+    //  console.log(this.indexToPosition(figurePos[0]));
+      
    }
  };
 
@@ -81,8 +92,8 @@ class App extends Component {
   };
  
   indexToPosition(index){
-    let xFind = parseInt(index / 20);
-    let yFind = index - xFind * 20;
+    let xFind = parseInt(index / 10);
+    let yFind = index - xFind * 10;
     return {X: xFind, Y: yFind, index: xFind + "" + yFind};
   };
 
