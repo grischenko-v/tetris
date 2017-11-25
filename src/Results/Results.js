ef0b50e934cbb80a48be9868fba18ef9aee8a280
Vladimir <boooobka@gmail.com>
2017-11-25 17:49:38 +0400

    next figure grid added

--- src/Results/Results.js
+++ src/Results/Results.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import './Results.css';
-
+import Element from '../Element/Element';
 
 class Results extends Component {
   constructor(props){
     super(props);  
-       console.log(this.props.result)
+    this.sizeX = 4;
+    this.sizeY = 4;
   };
-     
+  
+  indexToPosition(index){
+    let xFind = parseInt(index / 10, 10);
+    let yFind = index - xFind * 10;
+    return {X: xFind, Y: yFind, index: xFind + "" + yFind};
+  };
+
+  createGrid(){   
+   let elements = [];  
+   let temp;  
+   for(let i = 0; i < this.sizeX * this.sizeY; i++){
+     temp = this.indexToPosition(i);   
+     elements.push(<Element posX = {temp.X} posY = {temp.Y} key = {i} active = {false}/>);
+   }  
+   return elements;
+ };
+
   render() {  	
+  	const elements = this.createGrid();	
     return (
       <div className = "resultsblock">
          <div className = "resualt">{this.props.result}</div>
-         <div className = "nextfigure"></div>
+         <div className = "nextfigure_text">Next Figure:</div>
+         <div className = "nextfigure">        
+            {elements}
+         </div>
       </div>
     );
   }
