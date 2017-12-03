cd677159eb41df1ef324ec86894117f65754d115
Vladimir <boooobka@gmail.com>
2017-12-03 11:54:46 +0400

    add main menu

---
+++ src/MainMenu/MainMenu.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import './MainMenu.css';
+
+class MainMenu extends Component {
+  constructor(props){
+    super(props);  
+    
+    this.state = {  	
+  		grid: 1  		
+  	};
+  };
+
+  render() {  	
+  	
+    return (
+      <div className = "mainmenu">
+           <ul>      
+          <li>
+            <Link to={`/game`}           
+            style={{ textDecoration: 'none',
+                     color:'#888', 
+                     fontSize: '35px'
+                  }}>Play</Link>
+          </li>
+          <li>
+            <Link to={``} 
+            style={{ textDecoration: 'none',
+                     color:'#888',
+                     fontSize: '35px' 
+            }}>Resualts</Link>
+          </li>
+          <li>
+            <Link to={``} 
+            style={{ textDecoration: 'none',
+                     color:'#888',
+                     fontSize: '35px' 
+            }}>About</Link>
+          </li>     
+     
+    </ul>
+      </div>
+    );
+  }
+}
+
+export default MainMenu;
