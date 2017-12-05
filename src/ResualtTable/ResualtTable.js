eb456554d38f318bd41ff8f6e7d6329dd5f560a7
Vladimir <boooobka@gmail.com>
2017-12-05 22:23:03 +0400

    add table

--- src/ResualtTable/ResualtTable.js
+++ src/ResualtTable/ResualtTable.js
@@ -11,18 +11,33 @@ class ResualtTable extends Component {
      
   render() {  
     let rows = [];	
+
+    for (var i = 0; i < 10; i++){
+      let rowID = `row${i}`
+      let cell = []
+      for (var idx = 0; idx < 2; idx++){
+        let cellID = `cell${i}-${idx}`
+        cell.push(<td key={cellID} id={cellID}></td>)
+      }
+      rows.push(<tr key={i} id={rowID}>{cell}</tr>)
+    }
     return (
       <div className = "resualtable">
-         <table>
-
-         </table>
-
-         <Link to={`/#`}           
+          <table id="simple-board">
+               <tbody>
+                 {rows}
+               </tbody>
+             </table>
+          
+            <Link to={`/`}           
             style={{ textDecoration: 'none',
-                     color:'#eee', 
-                     fontSize: '35px'
-                  }}
-          >Main Menu</Link>
+                     color:'#fff', 
+                     fontSize: '35px',                     
+                     float: 'left',
+                     marginTop: '50px',
+                     marginLeft: '45px'
+                  }}>Back</Link>
+       
          
       </div>
     );
