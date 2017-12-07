b8275793f160e8e84aec632534041d32d192f388
Vladimir <boooobka@gmail.com>
2017-12-07 22:32:21 +0400

    addResualt add

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MainMenu from './MainMenu/MainMenu'
 import ResualtRoute from './ResualtRoute/ResualtRoute'
 import AboutRoute from './AboutRoute/AboutRoute'
 import ResualtTable from './ResualtTable/ResualtTable'
-
+import Element from './Element/Element'
 
 import './App.css';
 
@@ -13,7 +13,7 @@ class App extends Component {
 
   constructor(props) {
         super(props);        
-        this.setScore = this.setScore.bind(this);
+
         let initialResualts = {
            0: {name: 'andy',   score: 10000 },
            1: {name: 'robert', score:  9000 },
@@ -31,13 +31,48 @@ class App extends Component {
             score: 0,
             table: initialResualts
         };
-    }
+        this.setScore = this.setScore.bind(this);
+        this.addResualt = this.addResualt.bind(this);
+    };
+
+   addResualt(newResualt){     
+     let newResualts = this.cloneFigure(this.state.table);
+     let newResPosition;
+     for (let i = 0; i < 10; i++) {   
+       if(this.state.table[i].score 
+        > newResualt.score )
+           newResualts[i] = this.state.table[i];
+       else {
+           newResualts[i] = newResualt;
+           newResPosition = i + 1;
+           console.log(newResPosition);
+           break;
+       }
+     }
+     for (let i = newResPosition; i < 10; i++) {
+        newResualts[i] = this.state.table[i - 1];
+     }
+     this.setState({
+             table: newResualts
+        });
+
+   };
 
    setScore(value) {
         this.setState({
             score: value
         });
+    };
+
+
+  cloneFigure(obj) {
+    if (null == obj || "object" !== typeof obj) return obj;
+    var copy = obj.constructor();
+    for (var attr in obj) {
+        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
     }
+    return copy;
+ };
 
  render() { 
    return (
@@ -47,7 +82,7 @@ class App extends Component {
 
       <Route exact path='/' component={MainMenu}/>
       <Route path='/game' exact render={(props) => (<TetrisGrid setScore={this.setScore} {...props}/>)}/>
-      <Route path='/gameend' exact render={(props) => (<ResualtRoute score={this.state.score} {...props}/>)} />  
+      <Route path='/gameend' exact render={(props) => (<ResualtRoute score={this.state.score} addResualt={this.addResualt} {...props}/>)} />  
       <Route path='/about' component={AboutRoute}/>
       <Route path='/table' exact render={(props) => (<ResualtTable resualtTable={this.state.table} {...props}/>)} />         
     </Switch>    
